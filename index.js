bd2e95d349b03229718b2d768738b2aa2da10935
Carlos Arango <arangotorcar@gmail.com>
2021-09-06 16:51:34 -0500

    update

--- index.js
+++ index.js
@@ -1,55 +1,24 @@
-const csv = require('csv-parser');
 const fs = require('fs');
-const admin = require('firebase-admin');
+const csv = require('csv-parser');
+var admin = require('firebase-admin');
+
 const results = [];
-const serviceAccount = require('./config/firebase.json');
-const file = 'users.csv';
+const serviceAccount = require('./config/firebase_key.json');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const returnFlattenObject = (arr) => {
-  const flatObject = {};
-  for (let i = 0; i < arr.length; i++) {
-    for (const property in arr[i]) {
-      flatObject[`${property}`] = arr[i][property];
-    }
-  }
-  return flatObject;
+const db = admin.firestore();
+
+const importData = (results = []) => {
+  const docRef = db.collection('matches').doc('7vtBm1Zrd8cqhwZOO9MX');
+  docRef.set({ detPart: results }, { merge: true });
 };
 
-const importUsers = (users = []) => {
-  const db = admin.firestore();
-  const usersCollection = db.collection('users');
-  users.forEach((user = {}) => {
-    const array = [];
-    Object.keys(user).forEach((key) => {
-      const splittedKey = key.split('.');
-      if (splittedKey.length > 1) {
-        const keyObject = splittedKey[0];
-        const value = splittedKey[1];
-        array.push({ [value]: user[key] });
-      }
-      user.dept = [returnFlattenObject(array)];
-    });
-
-    usersCollection.add(user);
-
-    /*     if (splittedKey.length > 1) {
-      dep.push();
-    } */
-  });
-};
-
-fs.createReadStream(file)
+fs.createReadStream('detallPartido1.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    importUsers(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
+    importData(results);
   });
-
-console.log('Working...');
